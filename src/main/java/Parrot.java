90c18e0957cce81c2194a4c12f9defe0416d75ba
Kevin Childs <kchilds2020@gmail.com>
2023-01-03 14:47:36 -0600

    first commit

---
+++ src/main/java/Parrot.java
@@ -0,0 +1,17 @@
+
+public class Parrot {
+    /**
+     * This method should return whatever String was passed into it by whatever invoked it (in this case, the test),
+     * instead of returning "return text please!"
+     *
+     * for example:
+     * String str = copy("hello");
+     * System.out.println(str); -> prints out "hello"
+     *
+     * @param text is a parameter passed into this method by whatever called it.
+     * @return text.
+     */
+    public String copy(String text){
+        return "return text please!";
+    }
+}
