d86db51c20cdb4214f3cf273ab9ebdd75fe9e408
tedbeast <theodore.balashov@revature.com>
2023-01-17 12:36:46 -0500

    added docs that provide possible solutions for student to guess

--- src/main/java/Parrot.java
+++ src/main/java/Parrot.java
@@ -8,6 +8,20 @@ public class Parrot {
      * String str = copy("hello");
      * System.out.println(str); -> prints out "hello"
      *
+     * You may notice that replacing the String "return text please!" with "hello" or "polly" will work for one
+     * test, but will fail the other. This is because the test cases are setting up different scenarios to test the
+     * functionality of this method - it will always expect the method to return the same variable that was provided
+     * to the method. You can also try experimenting with the following guesses, as one of them will be the correct 
+     * answer. Please note that many of these will not compile, as evidenced by a "red line" under your code.
+     *
+     * return "hello"; | return String "hello";
+     * return hello;   | return String hello;
+     * return "polly"; | return String "polly";
+     * return polly;   | return String polly;
+     * return "text";  | return String "text";
+     * return text;    | return String text;
+     * return;         | return String;
+     *
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
